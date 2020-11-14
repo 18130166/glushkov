a625d191e0e1eba42edb099c96f6a0f6fb29e6ad
18130166 <57170698+18130166@users.noreply.github.com>
2020-11-14 22:40:13 +0500

    Create new.php

---
+++ bd_bank/bank/new.php
@@ -0,0 +1,25 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Глушков А.В. </title>
+
+
+<H2>Внесите данные:</H2>
+<form action='save_new.php' metod='get'> <p style='margin-left: 50px;'> <table style='font-size: 20'>
+<tr><td>Наименование:        <td><input name='name'      maxlength='20' type='text'></tr>
+<tr><td>ИНН:                 <td><input name='inn'       maxlength='10' type='text'></tr>
+<tr><td>Страна:              <td><input name='country'   maxlength='30' type='text'></tr>
+<tr><td>Класс надежности:    <td><input name='class'     maxlength='20' type='text'></tr>
+<tr><td>Объем активов:       <td><input name='assets'    maxlength='20' type='text'></tr>
+</table></p>
+<input name='add' type='submit' value='Добавить'>
+<input name='b2' type='reset' value='Очистить'>
+</form>
+    
+<p><a href="index.php"> ⬅Вернуться к списку </a>
+
+<style>
+    input [type='text']{
+    width: 300px;
+    height: 30px;
+    font-size: 16
+   }
+</style>
