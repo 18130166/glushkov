c9075d4d339c8d8536f13b2775dee0ef46bc2981
18130166 <57170698+18130166@users.noreply.github.com>
2020-11-14 22:52:16 +0500

    Update delete.php

--- bd_bank/bank_pog/delete.php
+++ bd_bank/bank_pog/delete.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'connect.php';
 $link = mysqli_connect($host, $user, $password, $database) or die ("Невозможно подключиться к серверу");
-$zapros="DELETE FROM f0470376_glushkov.bank WHERE id_bank=" . $_GET['id_bank'];
+$zapros="DELETE FROM f0470376_glushkov.bank_prog WHERE prog_id=" . $_GET['prog_id'];
 mysqli_query($link, $zapros);
 header("Location: index.php");
 exit;
