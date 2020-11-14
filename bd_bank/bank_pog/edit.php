1cbb78207b37325a84d929fc1247fdbe6acbb5cd
18130166 <57170698+18130166@users.noreply.github.com>
2020-11-14 22:52:19 +0500

    Update edit.php

--- bd_bank/bank_pog/edit.php
+++ bd_bank/bank_pog/edit.php
@@ -3,24 +3,29 @@
 <?php
 require_once 'connect.php';
 $link = mysqli_connect($host, $user, $password, $database) or die ("Невозможно подключиться к серверу");
-$query = "SELECT bank_name, INN, bank_country, 	bank_class, bank_assets FROM bank WHERE id_bank=".$_GET['id_bank'];
+$query = "SELECT * FROM bank_prog WHERE prog_id=".$_GET['prog_id'];
 $rows = mysqli_query($link, $query) or die ("Ошибка в запросе");
 while ($st = mysqli_fetch_array($rows)) {
-$id = $_GET['id_bank'];
-$name = $st['bank_name'];
-$inn = $st['INN'];
-$country = $st['bank_country'];
-$class = $st['bank_class'];
-$assets = $st['bank_assets'];
+$id = $_GET['prog_id'];
+$name = $st['prog_name'];
+$per = $st['prog_per'];
+$id_bank = $st['id_bank'];
 }
 print "<H2>Изменить:</H2>";
-print "<form action='save_edit.php' metod='get'><p style='margin-left: 50px;'><table style='font-size: 20'>";
-print "<tr><td>Наименование:        <td><input name='name'      maxlength='20' type='text' value='".$name."'></tr>";
-print "<tr><td>ИНН:                 <td><input name='inn'       maxlength='10' type='text' value='".$inn."'></tr>";
-print "<tr><td>страна:              <td><input name='country'   maxlength='30' type='text' value='".$country."'></tr>";
-print "<tr><td>Класс надежности:    <td><input name='class'     maxlength='20' type='text' value='".$class."'></tr>";
-print "<tr><td>Объем активов:       <td><input name='assets'    maxlength='20' type='text' value='".$assets."'></tr>";
+print "<form action='save_edit.php' metod='GET'><p style='margin-left: 50px;'><table style='font-size: 20'>";
 
+print "<tr><td>Название:    <td><input name='name'      maxlength='20' type='text' value='".$name."'></tr>";
+print "<tr><td>% годовых:   <td><input name='per'       maxlength='10' type='text' value='".$per."'></tr>";
+
+$query_1 = "SELECT * FROM f0470376_glushkov.bank";
+$rows_1 = mysqli_query($link, $query_1) or die ("Ошибка в запросе");
+   echo("<tr><td>id банка:<td><select id='prog_id' name='prog_id' >");
+    echo("<option disabled>Выберите</option>");
+     while($row = mysqli_fetch_array($rows_1)){
+        echo("<option value='$row[0]'> $row[0] - $row[1]</option>");
+     }
+     echo "</tr>";
+print " </select></p>";
 print "</tr></table></p>";
 print "<input type='hidden' name='id' value='".$id."'> <br>";
 print "<input type='submit' name='' value='Сохранить'>";
