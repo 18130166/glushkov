20e0d4d74a46b5f02baa2406904dd565057de1d4
18130166 <57170698+18130166@users.noreply.github.com>
2020-11-14 22:52:32 +0500

    Update save_edit.php

--- bd_bank/bank_pog/save_edit.php
+++ bd_bank/bank_pog/save_edit.php
@@ -2,21 +2,17 @@
 <?php
 require_once 'connect.php';
 $link = mysqli_connect($host, $user, $password, $database) or die ("Невозможно подключиться к серверу");
-if(isset($_GET['id'])&&isset($_GET['name'])&&isset($_GET['inn'])&&isset($_GET['country'])&&isset($_GET['class'])&&isset($_GET['assets'])){
+if(isset($_GET['id'])&&isset($_GET['name'])&&isset($_GET['per'])&&isset($_GET['id_bank'])){
     $name    = htmlentities(mysqli_real_escape_string($link, $_GET['name']));
-    $inn     = htmlentities(mysqli_real_escape_string($link, $_GET['inn']));
-    $country = htmlentities(mysqli_real_escape_string($link, $_GET['country']));
-    $class   = htmlentities(mysqli_real_escape_string($link, $_GET['class']));
-    $assets  = htmlentities(mysqli_real_escape_string($link, $_GET['assets']));
+    $per     = htmlentities(mysqli_real_escape_string($link, $_GET['per']));
+    $id_bank = htmlentities(mysqli_real_escape_string($link, $_GET['id_bank']));
     $id      = htmlentities(mysqli_real_escape_string($link, $_GET['id']));
     $zapros="UPDATE
-    f0470376_glushkov.bank
-    SET bank_name='$name',
-    INN='$inn',
-    bank_country='$country',
-    bank_class='$class',
-    bank_assets='$assets'
-    WHERE id_bank='$id'";
+    f0470376_glushkov.bank_prog
+    SET prog_name='$name',
+    prog_per='$per',
+    id_bank='$id_bank'
+    WHERE prog_id='$id'";
     mysqli_query($link, $zapros) or die("Запрос");
     if(mysqli_affected_rows($link)>0) {
         echo 'Все сохранено. <a href="index.php"> Назад </a>';
