72495dc806bdea578cc5ffdf61b61013ef65a68e
18130166 <57170698+18130166@users.noreply.github.com>
2020-11-14 23:00:17 +0500

    Update index5.php

--- bd_bank/index5.php
+++ bd_bank/index5.php
@@ -8,5 +8,9 @@
 <ul type="square">
 
 <li> <a href="bank/index.php">Банки</a></li> 
-
+<li> <a href="bank_pog/index.php">Программы</a></li> 
+<li> <a href="bank_vklad/index.php">Вклады</a></li> 
+<li> <a href="bank/gen_PDF.php">PDF</a></li> 
+<li> <a href="bank/gen_xls.php">Excel</a></li> 
+<li> <a href="txt.php">Посмотреть код</a></li> 
 </ul>
